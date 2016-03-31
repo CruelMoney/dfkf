3022d7071d4bebf7a3dd12df79e33d92659381cc
CruelMoney <chrdengso@gmail.com>
2016-03-31 19:04:38 +0200

    testing frontpaage images

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
 $(document).ready(function() {
-  $("#nav-trigger").click(function() {
-    $(".mobile-navigation").find("li:not(.li-active)").find("ul").hide(1);
-    $(".mobile-navigation").toggleClass("nav-active");
-  });
-  $('#search-toggle').click(function () {
-    var input = $('#search-box');
-    input.toggleClass('expanded');
-  });
-  $('.mobile-navigation a').click(function () {
-    console.log(this);
-    $(this).parent().parent().find("ul").hide(200);
-    $(this).parent().find("ul").show(200);
-
-  });
 
 // ObjectFit Fallback
 if ('objectFit' in document.documentElement.style === false) {
